8f02992e9a6299fc9510ff6023064ce97ffa0d81
PRASHOB P <prashobp2003.com>
2023-05-23 12:01:39 +0530

    first commit

---
+++ DAY 2/script 2.js
@@ -0,0 +1,12 @@
+function validation(){
+    let p=document.getElementById("pass").value
+    let p1=document.getElementById("pass1").value
+   
+    if(p==p1){
+        alert("Same")
+    }
+    else{
+     alert("Wrong")
+
+}
+}
